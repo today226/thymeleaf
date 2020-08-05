d76c5ebc119c83274e4a803ebd739a7e05f0a295
DenisOh <today226@nate.com>
2020-08-05 23:17:55 +0900

    최초 프로젝트 셋팅

---
+++ src/test/java/com/denis/thymeleaf/thymeleaf/ThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.denis.thymeleaf.thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
