3b914b4d91219bd59bed20c18d32833c572a33a3
DenisOh <today226@nate.com>
2020-08-07 02:52:19 +0900

    MariaDB 설정 추가 및 게시판 목록 조회

--- src/main/java/com/denis/thymeleaf/thymeleaf/controller/BoardController.java
+++ src/main/java/com/denis/thymeleaf/thymeleaf/controller/BoardController.java
@@ -1,16 +1,27 @@
 package com.denis.thymeleaf.thymeleaf.controller;
 
+import com.denis.thymeleaf.thymeleaf.model.Board;
+import com.denis.thymeleaf.thymeleaf.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
 
-    @GetMapping("/list")
-    public String list(){
+    @Autowired
+    private BoardRepository boardRepository;
 
+    @GetMapping("/list")
+    public String list(Model model){
+
+        List<Board> boardList = boardRepository.findAll();
+        model.addAttribute("boardList", boardList);
         return "board/list";
     }
 }
